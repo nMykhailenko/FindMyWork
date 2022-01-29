7d40c152ee54f640aeb36d0f19e9b7306562ae93
nmykhailenko <nickitamykhailneko@gmail.com>
2022-01-28 19:13:10 -0500

    initial

---
+++ src/modules/users/FindMyWork.Modules.Users.Core/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using FindMyWork.Modules.Users.Core.Domain.Enums;
+
+namespace FindMyWork.Modules.Users.Core.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Email { get; set; } = null!;
+    
+    public UserType UserType { get; set; }
+    
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
---
+++ src/modules/users/FindMyWork.Modules.Users.Core/Domain/Enums/UserType.cs
@@ -0,0 +1,9 @@
+﻿namespace FindMyWork.Modules.Users.Core.Domain.Enums;
+
+public enum UserType : short
+{
+    None = 0,
+    Employer = 1,
+    Employee = 2,
+    Manager = 3
+}
